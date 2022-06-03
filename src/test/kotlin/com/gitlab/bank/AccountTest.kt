bc48d6a058173060829dba83f077fbc29ae6c4a8
Dimitry ERNOT <dimitry.ernot@gmail.com>
2022-06-03 23:08:18 +0200

    open an account

--- src/test/kotlin/com/gitlab/bank/AccountTest.kt
+++ src/test/kotlin/com/gitlab/bank/AccountTest.kt
@@ -3,13 +3,16 @@ package com.gitlab.bank
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
+
+class Account {
+    val amount = Amount(0.0)
+}
+
 class AccountTest {
 
     @Test
     fun `a new account should be empty`() {
-        TODO("need amount")
-        /*val account = Account()
-
-        assertThat(account.amount).isEqualTo(Amount(0.0))*/
+        val account = Account()
+        assertThat(account.amount).isEqualTo(Amount(0.0))
     }
 }
