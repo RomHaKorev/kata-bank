787e1238707f8c35d934c11a8f330d8d5b6b552b
Dimitry ERNOT <dimitry.ernot@gmail.com>
2022-06-03 23:49:26 +0200

    can make a deposit on an account

--- src/test/kotlin/com/gitlab/bank/AccountTest.kt
+++ src/test/kotlin/com/gitlab/bank/AccountTest.kt
@@ -5,7 +5,15 @@ import org.junit.jupiter.api.Test
 
 
 class Account {
-    val amount = Amount(0.0)
+
+    private val deposits = mutableListOf<Deposit>()
+
+    val amount: Amount
+        get() = deposits.fold(Amount(0.0)) { acc, deposit -> Amount(acc.value + deposit.amount.value)}
+
+    fun make(deposit: Deposit) {
+        deposits.add(deposit)
+    }
 }
 
 class AccountTest {
@@ -18,11 +26,10 @@ class AccountTest {
 
     @Test
     fun `a deposit should increase the amount of an account`() {
-        TODO("need deposit")
-        /*val account = Account()
+        val account = Account()
 
-        account.`make`(Deposit.of(12.0))
+        account.make(Deposit(of= Amount(12.0)))
 
-        assertThat(account.amount).isEqualTo(Amount(12.0))*/
+        assertThat(account.amount).isEqualTo(Amount(12.0))
     }
 }
