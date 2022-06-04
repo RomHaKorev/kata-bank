210f604d77645540beec8e179a83beb25350149e
Dimitry ERNOT <dimitry.ernot@gmail.com>
2022-06-04 21:14:35 +0200

    write domain service RED test

--- src/test/kotlin/com/gitlab/bank/domain/FoobarTest.kt
+++ src/test/kotlin/com/gitlab/bank/domain/FoobarTest.kt
@@ -1,17 +1,44 @@
 package com.gitlab.bank.domain
 
+import com.gitlab.bank.domain.model.Account
 import com.gitlab.bank.domain.model.Amount
+import com.gitlab.bank.domain.model.BankClient
 import com.gitlab.bank.domain.model.Deposit
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+
+
+interface BankAccounts {
+    fun of(client: BankClient): Account
+}
+
+class MakeADeposit(val bankAccounts: BankAccounts) {
+    operator fun invoke(client: BankClient, deposit: Deposit) {
+
+    }
+}
+
+class InMemoryBankAccounts: BankAccounts {
+    val accounts = mutableMapOf<BankClient, Account>()
+
+    override fun of(client: BankClient) = accounts[client]!!
+    fun create(client: BankClient) {
+        accounts[client] = Account()
+    }
+
+}
 
 class FoobarTest {
     @Test
     fun `a client should make a deposit on their account`() {
-        /*val client = BankClient()
+        val bankAccounts = InMemoryBankAccounts()
+        val client = BankClient(named="Grace Slick")
+        bankAccounts.create(client)
 
-        val makeADeposit = MakeADeposit()
+        val makeADeposit = MakeADeposit(bankAccounts)
 
-        makeADeposit(client, Deposit(of = Amount(120.0)))*/
-        TODO("need BankClient")
+        makeADeposit(client, Deposit(of = Amount(120.0)))
+
+        assertThat(bankAccounts.of(client).amount).isEqualTo(Amount(120.0))
     }
 }
