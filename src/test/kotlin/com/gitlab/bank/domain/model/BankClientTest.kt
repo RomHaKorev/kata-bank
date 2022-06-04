57308947c21a926e3dd62c5ec9d3dba72a193723
Dimitry ERNOT <dimitry.ernot@gmail.com>
2022-06-04 19:16:49 +0200

    make bank client as entities

--- src/test/kotlin/com/gitlab/bank/domain/model/BankClientTest.kt
+++ src/test/kotlin/com/gitlab/bank/domain/model/BankClientTest.kt
@@ -6,6 +6,14 @@ import org.junit.jupiter.api.Test
 
 class BankClient(named: String) {
     val name = named
+
+    override fun equals(other: Any?): Boolean {
+        return other is BankClient && this.name == other.name
+    }
+
+    override fun hashCode(): Int {
+        return name.hashCode()
+    }
 }
 
 class BankClientTest {
@@ -15,5 +23,14 @@ class BankClientTest {
         val client = BankClient(named="Grace Slick")
 
         assertThat(client.name).isEqualTo("Grace Slick")
+    }
+
+    @Test
+    fun `a bank client should be identified by their name`() {
+        assertThat(BankClient(named="Grace Slick")).isEqualTo(BankClient(named="Grace Slick"))
+        assertThat(BankClient(named="Grace Slick")).isNotEqualTo(BankClient(named="Alison Mosshart"))
+
+        assertThat(BankClient(named="Grace Slick").hashCode()).isEqualTo(BankClient(named="Grace Slick").hashCode())
+        assertThat(BankClient(named="Grace Slick").hashCode()).isNotEqualTo(BankClient(named="Alison Mosshart").hashCode())
     }
 }
