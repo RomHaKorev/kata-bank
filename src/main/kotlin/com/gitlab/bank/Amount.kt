2c47db998d6ea26071414e64bf651c205f6aee44
Dimitry ERNOT <dimitry.ernot@gmail.com>
2022-06-04 15:54:32 +0200

    an amount of money is always positive

--- src/main/kotlin/com/gitlab/bank/Amount.kt
+++ src/main/kotlin/com/gitlab/bank/Amount.kt
@@ -1,6 +1,9 @@
 package com.gitlab.bank
 
 data class Amount(val value: Double) {
+    init {
+        assert(value >= 0.0) { "an amount cannot be negative" }
+    }
     operator fun plus(other: Amount): Amount {
         return Amount(value + other.value)
     }
--- src/test/kotlin/com/gitlab/bank/AmountTest.kt
+++ src/test/kotlin/com/gitlab/bank/AmountTest.kt
@@ -2,6 +2,8 @@ package com.gitlab.bank
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import java.lang.AssertionError
 
 
 class AmountTest {
@@ -14,5 +16,13 @@ class AmountTest {
     fun `an amount should be summed to another`() {
 
         assertThat(Amount(12.0) + Amount(16.0)).isEqualTo(Amount(28.0))
+    }
+
+
+    @Test
+    fun `an amount should always be positive`() {
+        assertThrows<AssertionError> {
+            Amount(-100.0)
+        }
     }
 }
